5bde69b4b637e9f49af94203dd0c32064d383063
PrfsJ <19522476@gm.uit.edu.vn>
2022-10-20 20:40:05 +0700

    update mongo connection

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try {
-        await mongoose.connect('mongodb://localhost:27017/web-app');
+        await mongoose.connect('mongodb+srv://admin:adgjm12345@cluster0.aqb89rw.mongodb.net/web-app');
         console.log("Connect to DB successfully!");
       } catch (error) {
         console.log(error);
